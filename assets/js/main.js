d4d83523dd0b2f0b246578c29927f7a77c27cb99
chytran <chytran@users.noreply.github.com>
2021-06-17 13:45:10 -0700

    Getting pokimon api connection

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,17 @@
 // Store API
 var api = {
-    link: "https://pokeapi.co/api/v2/"
+    link: "https://pokeapi.co/api/v2/pokemon"
 }
 
 // Get api through JSON
 
 
-
 // Get pokemon on search
-
+const getPokemon = async id => {
+    const url = `https://pokeapi.co/api/v2/pokemon${id}`;
+    const res = await fetch(url)
+    const pokemon = await res.json();
+    console.log(pokemon);
+}
 
 
