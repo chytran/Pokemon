eb2549240cfd24827819fdcf3fa1578a0e6832b6
chytran <chytran@users.noreply.github.com>
2021-06-18 10:57:26 -0700

    Making a loop to get multiple pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,36 @@ var api = {
     link: "https://pokeapi.co/api/v2/pokemon"
 }
 
-// Get api through JSON
+// Variables
+const container = document.querySelector(".container");
 
+// Fetch Pokemon
+const fetchPokemon = async () => {
+    for (let i = 1; i <= 30; i++){
+        await getPokemon(i);
+    }
+}
 
 // Get pokemon on search
 const getPokemon = async id => {
-    const url = `https://pokeapi.co/api/v2/pokemon${id}`;
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url)
     const pokemon = await res.json();
-    console.log(pokemon);
+    pokiCard(pokemon);
+}
+
+// Create pokemon information
+function pokiCard(pokemon) {
+    const pokeEl = document.createElement("div");
+    pokeEl.classList.add("pokiCard");
+
+    const pokiInfo = `
+        ${pokemon.id}
+    `;
+
+    pokeEl.innerHTML = pokiInfo;
+
+    container.appendChild(pokeEl);
 }
 
 
